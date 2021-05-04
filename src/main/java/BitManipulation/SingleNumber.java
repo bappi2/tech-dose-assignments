c22778401863299dc6f34ac3d556cf4a216a39d6
Mohammad Meftauddin <mmeftauddin@zulily.com>
2021-05-04 15:05:29 -0700

    single number and bubble sort

---
+++ src/main/java/BitManipulation/SingleNumber.java
@@ -0,0 +1,10 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num: nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int [] nums) {
+        int len = nums.length;
+        for (int i=0; i < len; i++) {
+            for (int j=1; j < len - i; j++) {
+                if (nums[j-1] > nums[j]) {
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int [] nums = { 10, 2, 5, 4, 1, 8, 7};
+        printArray( nums);
+
+        bubbleSort(nums);
+
+        printArray(nums);
+
+    }
+}
