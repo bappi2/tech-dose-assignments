00c2adf8a839ffc9426997b472cbe8bf92c9e76c
Mohammad Meftauddin <mmeftauddin@zulily.com>
2021-05-05 10:48:17 -0700

    hammingDistance

--- src/BitManipulation/HammingDistance.java
+++ src/BitManipulation/HammingDistance.java
@@ -1,4 +1,13 @@
 package BitManipulation;
 
 public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+        for (int i=0; i < Integer.SIZE; i++) {
+            if ((x & (1 << i) ^ y & (1 << i)) != 0) {
+                result++;
+            }
+        }
+        return result;
+    }
 }
