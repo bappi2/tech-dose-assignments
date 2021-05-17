0356e0efe40f0008ee8dee2d27ab184d5b631229
Mohammad Meftauddin <mmeftauddin@zulily.com>
2021-05-17 15:54:42 -0700

    NumberOf1Bits

---
+++ src/BitManipulation/NumberOf1Bits.java
@@ -0,0 +1,24 @@
+package BitManipulation;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
+    }
+
+    public int hammingWeightBetter(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
