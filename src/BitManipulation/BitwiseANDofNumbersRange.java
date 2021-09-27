c65ea12730a2d3b9fb0efb32846c193177889487
Mohammad Meftauddin <mmeftauddin@zulily.com>
2021-09-27 10:19:44 -0700

    add

--- src/BitManipulation/BitwiseANDofNumbersRange.java
+++ src/BitManipulation/BitwiseANDofNumbersRange.java
@@ -1,6 +1,8 @@
 package BitManipulation;
 
 public class BitwiseANDofNumbersRange {
+    // Given two integers left and right that represent the range [left, right],
+    // return the bitwise AND of all numbers in this range, inclusive.
     public int rangeBitwiseAnd(int left, int right) {
         int shift = 0;
         while (left < right) {
@@ -9,5 +11,13 @@ public class BitwiseANDofNumbersRange {
             shift++;
         }
         return left << shift;
+    }
+
+    public int rangeBitwiseAnd2(int m, int n) {
+        while (m < n) {
+            // turn off rightmost 1-bit
+            n = n & (n - 1);
+        }
+        return m & n;
     }
 }
--- src/BitManipulation/CountingBits.java
+++ src/BitManipulation/CountingBits.java
@@ -1,6 +1,12 @@
 package BitManipulation;
 
 public class CountingBits {
+
+    // Given an integer n, return an array ans of length n + 1 such that for each
+    // i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+    // Input: n = 5
+    // Output: [0,1,1,2,1,2]
+
     public int[] countBits(int num) {
         int [] ans = new int [num +1];
         for (int i=0; i <= num; i++) {
@@ -22,6 +28,19 @@ public class CountingBits {
         ans[0] = 0;
         for (int i=1; i <= num; i++) {
             ans[i] = ans[i/2] + i % 2;
+        }
+        return ans;
+    }
+
+    // Last set bit is the rightmost set bit. Setting that bit to zero with
+    // the bit trick, x &= x - 1, leads to the following transition function:
+    //
+    //P(x) = P(x \mathrel{\&} (x - 1)) + 1P(x)=P(x&(x−1))+1
+
+    public int[] countBits2(int num) {
+        int[] ans = new int[num + 1];
+        for (int x = 1; x <= num; ++x) {
+            ans[x] = ans[x & (x - 1)] + 1;
         }
         return ans;
     }
