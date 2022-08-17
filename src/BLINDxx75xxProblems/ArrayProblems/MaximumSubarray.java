cfa630c46b42776244f32e4bc65200c00400fb23
Mohammad Meftauddin <mmeftauddin@it-seavhyt9-mb6.corp.zulily.com>
2022-08-17 16:40:54 -0700

    add code

---
+++ src/BLINDxx75xxProblems/ArrayProblems/MaximumSubarray.java
@@ -0,0 +1,37 @@
+package BLINDxx75xxProblems.ArrayProblems;
+
+public class MaximumSubarray {
+    // Time complexity: O(N^2)
+    // Space complexity: O(1)
+    public int maxSubArray(int[] nums) {
+        int maxSubarray = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSubarray = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSubarray += nums[j];
+                maxSubarray = Math.max(maxSubarray, currentSubarray);
+            }
+        }
+
+        return maxSubarray;
+    }
+    // Time complexity: O(N)
+    // Space complexity: O(1)
+    public int maxSubArray2(int[] nums) {
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+
+        return maxSubarray;
+    }
+
+
+}
