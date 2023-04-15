0230078e9754633e4355cc6e64deebcdcda07b58
MOHAMMAD MEFTAUDDIN <bappi@live.com>
2023-04-15 01:03:57 -0700

    Update LongestCommonSubstring.java
    Recursive

--- src/BLINDxx75xxProblems/DynamicProgramming/LongestCommonSubsequence/LongestCommonSubstring.java
+++ src/BLINDxx75xxProblems/DynamicProgramming/LongestCommonSubsequence/LongestCommonSubstring.java
@@ -22,3 +22,17 @@ public class LongestCommonSubstring {
         return lcs;
     }
 }
+
+public int lcs(int[] A, int[] B, int m, int n, int res) {
+        if (m == -1 || n == -1) {
+            return res;
+        }
+        if (A[m] == B[n]) {
+            res = lcs(A, B, m - 1, n - 1, res + 1);
+        }
+        return max(res, max(lcs(A, B, m, n - 1, 0), lcs(A, B, m - 1, n, 0)));
+    }
+
+    public int longestCommonSubString(int[] A, int[] B) {
+        return lcs(A, B, A.length - 1, B.length - 1, 0);
+    }
