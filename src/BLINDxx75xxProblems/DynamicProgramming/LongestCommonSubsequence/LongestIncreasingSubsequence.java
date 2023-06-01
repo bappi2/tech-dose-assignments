0d002096d8ad36cd660d09ee0d6fd26d8aaf3ac0
mohammad meftauddin <bappi@live.com>
2023-06-01 13:46:59 -0700

    LIS code

--- src/BLINDxx75xxProblems/DynamicProgramming/LongestCommonSubsequence/LongestIncreasingSubsequence.java
+++ src/BLINDxx75xxProblems/DynamicProgramming/LongestCommonSubsequence/LongestIncreasingSubsequence.java
@@ -24,4 +24,44 @@ public class LongestIncreasingSubsequence {
 
         return longest;
     }
+
+    public int lengthOfLISRecursive(int[] nums) {
+        return LISHelper(-1, 0, nums);
+    }
+
+    private static int LISHelper(int prev, int curr, int nums[]){
+        if(curr == nums.length) return 0;
+
+        int take = 0;
+        if(prev == -1 || nums[prev] < nums[curr]){
+            take =  1 + LISHelper(curr, curr+1,nums);
+        }
+        int notTake = LISHelper(prev, curr+1,nums);
+
+        return Math.max(take, notTake);
+    }
+
+    public int lengthOfLISRecursiveDP(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length+1];
+        for (int [] rows : dp) {
+            Arrays.fill(rows, -1);
+        }
+        return LISHelperDP(-1, 0, nums, dp);
+    }
+
+    private int LISHelperDP(int prev, int curr, int[] nums, int[][] dp) {
+        if (curr == nums.length) {
+            return 0;
+        }
+        if (dp[curr][prev + 1] != -1) {
+            return dp[curr][prev+1];
+        }
+        int take = 0;
+        if (prev == -1 || nums[prev] < nums[curr]) {
+            take = 1 + LISHelperDP(curr, curr +1, nums, dp);
+        }
+        int notTake = LISHelperDP(prev, curr+1, nums, dp);
+        return dp[curr][prev+1] = Math.max(take, notTake);
+    }
+
 }
