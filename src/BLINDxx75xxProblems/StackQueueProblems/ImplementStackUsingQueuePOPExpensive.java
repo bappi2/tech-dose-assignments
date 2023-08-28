e0b20b495a3124cacd7686f25b8e8d1a0e73f7e8
Mohammad Meftauddin <mmeftauddin@mohammads-mbp.lan>
2023-08-27 21:57:01 -0700

    add code

--- src/BLINDxx75xxProblems/StackQueueProblems/ImplementStackUsingQueuePOPExpensive.java
+++ src/BLINDxx75xxProblems/StackQueueProblems/ImplementStackUsingQueuePOPExpensive.java
@@ -1,50 +1,70 @@
 package BLINDxx75xxProblems.StackQueueProblems;
-
 import java.util.LinkedList;
 import java.util.Queue;
-public class ImplemenStackUsingQueue {
+
+class ImplementStackUsingQueuePOPExpensive {
         private Queue<Integer> q1 = new LinkedList<>();
         private Queue<Integer> q2 = new LinkedList<>();
-        private int currSize = 0;
+        private int currSize;
 
-        public void push(int x) {
-            currSize++;
-            q2.add(x);
-
-            while (!q1.isEmpty()) {
-                q2.add(q1.poll());
-            }
-
-            Queue<Integer> temp = q1;
-            q1 = q2;
-            q2 = temp;
+        public ImplementStackUsingQueuePOPExpensive() {
+            currSize = 0;
         }
 
         public void pop() {
             if (q1.isEmpty()) {
                 return;
             }
+
+            while (q1.size() != 1) {
+                q2.add(q1.poll());
+            }
+
             q1.poll();
             currSize--;
+
+            Queue<Integer> temp = q1;
+            q1 = q2;
+            q2 = temp;
+        }
+
+        public void push(int x) {
+            q1.add(x);
+            currSize++;
         }
 
         public int top() {
             if (q1.isEmpty()) {
                 return -1;
             }
-            return q1.peek();
+
+            while (q1.size() != 1) {
+                q2.add(q1.poll());
+            }
+
+            int temp = q1.peek();
+            q1.poll();
+            q2.add(temp);
+
+            Queue<Integer> tempQueue = q1;
+            q1 = q2;
+            q2 = tempQueue;
+
+            return temp;
         }
 
         public int size() {
             return currSize;
         }
+    }
 
     public class Main {
         public static void main(String[] args) {
-            ImplemenStackUsingQueue s = new ImplemenStackUsingQueue();
+            ImplementStackUsingQueuePOPExpensive s = new ImplementStackUsingQueuePOPExpensive();
             s.push(1);
             s.push(2);
             s.push(3);
+            s.push(4);
 
             System.out.println("current size: " + s.size());
             System.out.println(s.top());
@@ -52,8 +72,8 @@ public class ImplemenStackUsingQueue {
             System.out.println(s.top());
             s.pop();
             System.out.println(s.top());
-
             System.out.println("current size: " + s.size());
         }
     }
+
 }
